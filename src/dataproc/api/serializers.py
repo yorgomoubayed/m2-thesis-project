4340e76ea7a0db9ef02ba9eca4c9cd41aaadc1a7
Yorgo El Moubayed <yelmoubayed@embl.fr>
2021-03-04 05:54:10 +0000

    Deleted src/dataproc/__pycache__/__init__.cpython-38.pyc, src/dataproc/__pycache__/admin.cpython-38.pyc, src/dataproc/__pycache__/apps.cpython-38.pyc, src/dataproc/__pycache__/models.cpython-38.pyc, src/dataproc/api/__init__.py, src/dataproc/api/serializers.py, src/dataproc/api/urls.py, src/dataproc/api/views.py, src/dataproc/migrations/models/report.py, src/dataproc/migrations/models/rsf.py, src/dataproc/migrations/__init__.py, src/dataproc/tests/__init.py__, src/dataproc/tests/test_forms.py, src/dataproc/tests/test_models.py, src/dataproc/tests/test_urls.py, src/dataproc/tests/test_views.py, src/dataproc/__init__.py, src/dataproc/admin.py, src/dataproc/apps.py, src/dataproc/models.py, src/dataproc/settings.py, src/dataproc/views.py, src/myservice/__pycache__/__init__.cpython-38.pyc, src/myservice/__pycache__/settings.cpython-38.pyc, src/myservice/__pycache__/urls.cpython-38.pyc, src/myservice/__init__.py, src/myservice/asgi.py, src/myservice/settings.py, src/myservice/urls.py, src/myservice/wsgi.py, src/manage.py files

--- src/dataproc/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from dataproc.models import Contruct
-
-
-class ContructSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Contruct
-		fields = ['title', 'created_at', 'updated_at',]
--- src/dataproc/tests/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-
-class TestUrls(SimpleTestCase):
-
-	def {test_url}(self):
-		url = reverse('{url_route}')
-		self.assertEquals(resolve(url).func, )
-

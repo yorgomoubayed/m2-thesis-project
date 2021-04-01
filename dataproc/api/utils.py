f0fcf80e5a4b6230fc54df60937ee6799b52f6b7
Yorgo El Moubayed <yelmoubayed@embl.fr>
2021-04-01 13:55:48 +0200

    Add utils files

---
+++ dataproc/api/utils.py
@@ -0,0 +1,20 @@
+from api.models.user_model import User
+from api.models.construct_model import Construct
+
+MODEL_ENTITIES = {
+'Construct': Construct,
+'User': User
+}
+
+def fetch_nodes(fetch_info):
+    node_type       = fetch_info['node_type']
+    search_word     = fetch_info['name']
+    country         = fetch_info['country']
+    limit           = fetch_info['limit']
+    start           = ((fetch_info['page'] - 1) * limit)
+    end             = start + limit
+    jurisdiction    = fetch_info['jurisdiction']
+    node_set        = filter_nodes(MODEL_ENTITIES[node_type], search_word, country, jurisdiction)
+    fetched_nodes   = node_set[start:end]
+
+    return [node.serialize for node in fetched_nodes]

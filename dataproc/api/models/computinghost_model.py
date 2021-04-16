f3a4c4a36c1773ee6f0618bee97829a5ce69da83
root <Yorgo EL MOUBAYED>
2021-04-16 15:40:53 +0200

    Update Computinghost model

--- dataproc/api/models/computinghost_model.py
+++ dataproc/api/models/computinghost_model.py
@@ -1,6 +1,9 @@
 from neomodel import StructuredNode, StringProperty, IntegerProperty,UniqueIdProperty, RelationshipTo
+from uuid import uuid4
 
 class ComputingHost(StructuredNode):
+	name=StringProperty()
+	uuid=StringProperty(unique_index=True, default=uuid4)
 	ch_softwares=StringProperty()
 	ch_softwares_number=IntegerProperty()
 
@@ -13,6 +16,8 @@ class ComputingHost(StructuredNode):
 		
 		return {
 			'node_properties': {
+				'name': self.name,
+				'uuid': self.uuid,
 				'ch_softwares': self.ch_softwares,
 				'ch_softwares_number': self.ch_softwares_number,
 			},

01625f2ff94f277b4ec10b2d24256d138757c597
root <Yorgo EL MOUBAYED>
2021-04-16 15:17:05 +0200

    Update Storagehost model

--- dataproc/api/models/storagehost_model.py
+++ dataproc/api/models/storagehost_model.py
@@ -1,8 +1,9 @@
 from neomodel import StructuredNode, StringProperty, IntegerProperty,UniqueIdProperty, RelationshipTo
+from uuid import uuid4
 
 class StorageHost(StructuredNode):
-	uuid=StringProperty()
 	name=StringProperty()
+	uuid=StringProperty(unique_index=True, default=uuid4)
 	sh_files=StringProperty()
 	sh_files_number=IntegerProperty()
 
@@ -15,6 +16,8 @@ class StorageHost(StructuredNode):
 		
 		return {
 			'node_properties': {
+				'name': self.name,
+				'uuid': self.uuid,
 				'sh_files': self.sh_files,
 				'sh_files_number': self.sh_files_number,
 			},

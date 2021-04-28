d1636931d1d48c42336d15b20ce8fd452c676558
root <Yorgo EL MOUBAYED>
2021-04-28 23:26:17 +0200

    Update computation view

---
+++ dataproc/api/views/computationhost_view.py
@@ -0,0 +1,99 @@
+"""
+api application controllers to perform CRUD operations on the ComputationHost model
+"""
+
+# Import python and django libraries
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+# Import models
+from api.models.computationhost_model import ComputationHost
+
+
+# Define CRUD functions
+@csrf_exempt
+def indexComputationhost(request):
+
+    """
+    Get all computation host
+    """
+
+    if request.method=='GET':
+        try:
+            computinghosts=ComputingHost.nodes.all()
+            response=[]
+            for computationhost in computationhosts :
+                node=computationhost.serialize
+                response.append(node)
+            return JsonResponse(response, safe=False)
+        except:
+            return JsonResponse({"error": "Error occurred"}, safe=False)
+
+@csrf_exempt
+def showComputationhost(request):
+
+    """
+    Get one computation host by name
+    """
+
+    if request.method=='GET':
+        name=request.GET.get('name', ' ')
+        try:
+            computationhost=ComputationHost.nodes.get(name=name)
+            return JsonResponse(computationhost.serialize, safe=False)
+        except :
+            return JsonResponse({"error":"Error occurred"}, safe=False)
+
+@csrf_exempt
+def storeComputationhost(request):
+
+    """
+    Create one computation host
+    """
+
+    if request.method=='POST':
+        json_data=json.loads(request.body)
+        name=json_data['name']
+        try:
+            computationhost=ComputationHost(name=name)
+            computationhost.save()
+            return JsonResponse(computationhost.serialize)
+        except :
+            return JsonResponse({"error":"Error occurred"}, safe=False)
+
+@csrf_exempt
+def updateComputationhost(request):
+
+    """
+    Update one computation host
+    """
+
+    if request.method=='PUT':
+        json_data=json.loads(request.body)
+        name=json_data['name']
+        uuid=json_data['uuid']
+        try:
+            computationhost=ComputationHost.nodes.get(uuid=uuid)
+            computationhost.name=name
+            computation.save()
+            return JsonResponse(computationhost.serialize, safe=False)
+        except:
+            return JsonResponse({"error":"Error occurred"}, safe=False)
+
+@csrf_exempt
+def destroyComputationhost(request):
+
+    """
+    Delete one computation host by name
+    """
+
+    if request.method=='DELETE':
+        json_data=json.loads(request.body)
+        name=json_data['name']
+        try:
+            computationhost=ComputationHost.nodes.get(name=name)
+            computationhost.delete()
+            return JsonResponse({"success": "computation host deleted"}, safe=False)
+        except:
+            return JsonResponse({"error":"Error occurred"}, safe=False)
